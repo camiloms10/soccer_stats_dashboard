f12aaade0847d0639eaba5fd39d8d79dc6493981
Camilo.Manzur <camilo.manzur@justo.mx>
2022-07-15 23:01:17 -0500

    Deleted de GCP comments section as I don't have a GCP account

--- apps/constants.py
+++
@@ -1,6 +0,0 @@
-SCOPE = "https://www.googleapis.com/auth/spreadsheets"
-SPREADSHEET_ID = "1zfoCfEytP7UDTx3rDxoeVA3ht4ObIikPDkhRwpVvnEs"
-SHEET_NAME = "Database"
-GSHEET_URL = f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}"
-COMMENT_TEMPLATE_MD = """{} - {}
-> {}"""
--- apps/model.py
+++ apps/model.py
@@ -10,7 +10,6 @@ import matplotlib.pyplot as plt
 import plotly.figure_factory as ff
 import plotly.express as px
 from datetime import datetime
-from apps import db
 
 def app():
 
@@ -104,41 +103,4 @@ def app():
 
     else:
         st.write("You didn't select any data you want to analyze.")
-
-
-# Comments part
-
-conn = db.connect()
-comments = db.collect(conn)
-
-with st.expander("💬 Open comments"):
-
-    # Show comments
-
-    st.write("**Comments:**")
-
-    for index, entry in enumerate(comments.itertuples()):
-        st.markdown(COMMENT_TEMPLATE_MD.format(entry.name, entry.date, entry.comment))
-
-        is_last = index == len(comments) - 1
-        is_new = "just_posted" in st.session_state and is_last
-        if is_new:
-            st.success("☝️ Your comment was successfully posted.")
-
-    space(2)
-
-    # Insert comment
-
-    st.write("**Add your own comment:**")
-    form = st.form("comment")
-    name = form.text_input("Name")
-    comment = form.text_area("Comment")
-    submit = form.form_submit_button("Add comment")
-
-    if submit:
-        date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        db.insert(conn, [[name, comment, date]])
-        if "just_posted" not in st.session_state:
-            st.session_state["just_posted"] = True
-        st.experimental_rerun()
 
