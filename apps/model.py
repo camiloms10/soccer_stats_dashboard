f2b95b92cf822c016a81d208e005c82cfca256c7
Camilo.Manzur <camilo.manzur@justo.mx>
2022-07-20 02:22:50 -0500

    Added new filter and scatter plot to the players section of the dashboard

--- apps/model.py
+++ apps/model.py
@@ -9,6 +9,7 @@ import plotly.express as px
 from datetime import datetime
 import math
 import streamlit.components.v1 as components
+import statsmodels.api as sm
 
 def app():
 
@@ -50,21 +51,22 @@ def app():
     df_games = pd.read_csv('dataset/games.csv')
     df_appearances = pd.read_csv('dataset/appearances.csv')
     df_players = pd.read_csv('dataset/players.csv')
+    df_player_valuations = pd.read_csv('dataset/player_valuations.csv')
 
 
     if dataset == "Clubs":
 
         #Adding competition names
-        df_games = pd.merge(df_games,df_competitions[['competition_id','name']], left_on = 'competition_code', right_on= 'competition_id')
+        df_games = pd.merge(df_games,df_competitions[['competition_id','name']],how='left', left_on = 'competition_code', right_on= 'competition_id')
 
         #Filtering only League competitions
         df_games = pd.merge(df_games,df_leagues[['league_id']],how='inner', left_on= 'competition_id' , right_on='league_id')
 
         #Adding home club names
-        df_games = pd.merge(df_games,df_clubs[['club_id','pretty_name']], left_on='home_club_id', right_on ='club_id')
+        df_games = pd.merge(df_games,df_clubs[['club_id','pretty_name']],how='left', left_on='home_club_id', right_on ='club_id')
 
         #Adding away club names
-        df_games = pd.merge(df_games,df_clubs[['club_id','pretty_name']], left_on='away_club_id', right_on ='club_id')
+        df_games = pd.merge(df_games,df_clubs[['club_id','pretty_name']],how='left', left_on='away_club_id', right_on ='club_id')
 
         #Changing pretty names column name
         df_games["Home_club_name"] = df_games['pretty_name_x']
@@ -113,8 +115,8 @@ def app():
 
             df_away_goals = df_games.groupby(["Away_club_name","season"]).sum().reset_index().sort_values("season")
 
-            fig_away_goals = px.line(df_away_goals, x="season",color="Away_club_name", y="Goals_For", title='Away Goals For by club', hover_data=["Goals_Against"], markers=True).update_xaxes(type='category')
-            fig_away_points = px.line(df_away_goals, x="season",color="Away_club_name", y="Away_points", title='Total Away points by club', markers=True).update_xaxes(type='category')       
+            fig_away_goals = px.line(df_away_goals, x="season",color="Away_club_name", y="Goals_For", title='Away Goals For by club', hover_data=["Goals_Against"], markers=True, text="Goals_For").update_xaxes(type='category').update_traces(textposition="top center")
+            fig_away_points = px.line(df_away_goals, x="season",color="Away_club_name", y="Away_points", title='Total Away points by club', markers=True,text="Away_points").update_xaxes(type='category').update_traces(textposition="top center")      
 
             df_away_goals_table = df_away_goals.pivot(index='Away_club_name', columns='season', values='Goals_For')
             df_away_points_table = df_away_goals.pivot(index='Away_club_name', columns='season', values='Home_points')
@@ -193,25 +195,25 @@ def app():
 
     else:
         #Adding competition names
-        df_appearances = pd.merge(df_appearances,df_competitions[['competition_id','name']], left_on = 'competition_id', right_on= 'competition_id')
+        df_appearances = pd.merge(df_appearances,df_competitions[['competition_id','name']],how='left', left_on = 'competition_id', right_on= 'competition_id')
 
         #Filtering only League competitions
         df_appearances = pd.merge(df_appearances,df_leagues[['league_id']],how='inner', left_on= 'competition_id' , right_on='league_id')
 
         #Adding home club names
-        df_appearances = pd.merge(df_appearances,df_clubs[['club_id','pretty_name']], left_on='player_club_id', right_on ='club_id')
+        df_appearances = pd.merge(df_appearances,df_clubs[['club_id','pretty_name']],how='left', left_on='player_club_id', right_on ='club_id')
 
         #Changing pretty names column name
         df_appearances["Club_name"] = df_appearances['pretty_name']
 
         #Adding home club names
-        df_appearances = pd.merge(df_appearances,df_players[['player_id','pretty_name','country_of_birth','position']], left_on='player_id', right_on ='player_id')
+        df_appearances = pd.merge(df_appearances,df_players[['player_id','pretty_name','country_of_birth','position']],how='left', left_on='player_id', right_on ='player_id')
 
         #Changing pretty names column name
         df_appearances["Player_name"] = df_appearances['pretty_name_y']
 
         #Adding season to df
-        df_appearances = pd.merge(df_appearances,df_games[['game_id','season']], left_on='game_id', right_on ='game_id')
+        df_appearances = pd.merge(df_appearances,df_games[['game_id','season']],how='left', left_on='game_id', right_on ='game_id')
 
         #Adding G+A column
         df_appearances['G+A'] = df_appearances['goals']+df_appearances['assists']
@@ -225,8 +227,14 @@ def app():
         competition_choice = st.sidebar.multiselect(
          'Select your competitions',competitions, default = "premier-league")
 
+        #Creating position selector
+        positions = df_appearances['position'].drop_duplicates()
+        position_choice = st.sidebar.multiselect(
+         'Select player positions',positions, default = positions)      
         
-        df_appearances = df_appearances.query("season >= @start_year").query("season <= @last_year").query("name == @competition_choice")
+
+        
+        df_appearances = df_appearances.query("season >= @start_year").query("season <= @last_year").query("name == @competition_choice").query("position == @position_choice")
 
         df_goals = df_appearances.groupby("Player_name").agg({'game_id':'count', 'goals': 'sum','assists':'sum','minutes_played':'sum','yellow_cards':'sum','red_cards':'sum'}).reset_index().rename(columns={'game_id':'games_played'})
 
@@ -241,15 +249,38 @@ def app():
         df_goals['G+A_per_90_min'] = df_goals['G+A']/df_goals['minutes_played']*90
 
 
-        fig_goals = px.bar(df_goals.sort_values('goals',ascending=False).head(25), x='Player_name', y='goals', title = "Top 25 goal scorers", color='goals')
-        fig_assists = px.bar(df_goals.sort_values('assists',ascending=False).head(25), x='Player_name', y='assists', title = "Top 25 assists leaders", color="assists")
-        fig_g_a= px.bar(df_goals.sort_values('G+A',ascending=False).head(25), x='Player_name', y='G+A', title = "Top 25 assists leaders", color = "G+A", hover_data=["goals","assists"])
+        fig_goals = px.bar(df_goals.sort_values('goals',ascending=False).head(25), x='Player_name', y='goals', title = "Top 25 goal scorers", color='goals', hover_data= ["goals_per_90_min"],text_auto=True)
+        fig_assists = px.bar(df_goals.sort_values('assists',ascending=False).head(25), x='Player_name', y='assists', title = "Top 25 assists leaders", color="assists", hover_data= ["assists_per_90_min"],text_auto=True)
+        fig_g_a= px.bar(df_goals.sort_values('G+A',ascending=False).head(25), x='Player_name', y='G+A', title = "Top 25 assists leaders", color = "G+A", hover_data=["goals","assists"],text_auto=True)
 
-        
+
         st.plotly_chart(fig_goals)
         st.write(df_goals[["Player_name","games_played","goals","goals_per_90_min","minutes_played"]].sort_values('goals',ascending=False).head(25).reset_index(drop = True))
         st.plotly_chart(fig_assists)
         st.dataframe(df_goals[["Player_name","games_played","assists","assists_per_90_min","minutes_played"]].sort_values('assists',ascending=False).head(25).reset_index(drop = True)) 
         st.plotly_chart(fig_g_a)
-        st.dataframe(df_goals[["Player_name","games_played","goals","assists","G+A","G+A_per_90_min","minutes_played"]].sort_values('G+A',ascending=False).head(25).reset_index(drop = True))      
+        st.dataframe(df_goals[["Player_name","games_played","goals","assists","G+A","G+A_per_90_min","minutes_played"]].sort_values('G+A',ascending=False).head(25).reset_index(drop = True))  
 
+        #Joining players stats by season and player_valuations by season in single df
+        df_appearances_by_season = df_appearances.groupby(["season","player_id","Player_name","country_of_birth","position"]).agg({'game_id':'count', 'goals': 'sum','assists':'sum','minutes_played':'sum','yellow_cards':'sum','red_cards':'sum'}).reset_index().rename(columns={'game_id':'games_played'})
+        df_appearances_by_season['G+A'] = df_appearances_by_season['goals']+df_appearances_by_season['assists']
+        df_appearances_by_season['G+A_per_90_min'] = df_appearances_by_season['G+A']/df_appearances_by_season['minutes_played']*90
+        df_player_valuations['date']=pd.to_datetime(df_player_valuations['date'])
+        df_player_valuations['date']=pd.DatetimeIndex(df_player_valuations['date']).year
+
+        #Getting last valuation per season per player_id
+        idx = df_player_valuations.groupby(["player_id","date"])['date'].idxmax()
+        df_player_valuations = df_player_valuations.loc[idx]
+     
+        df_player_valuations['season'] = df_player_valuations['date']
+        df_player_valuations = pd.merge(df_player_valuations,df_appearances_by_season, left_on=['season','player_id'], right_on =['season','player_id'])   
+
+        #Filtering minimum games played
+        df_player_valuations = df_player_valuations.loc[df_player_valuations['games_played'] > 12] 
+
+        fig_g_a_vs_mkt_value = px.scatter(df_player_valuations,x='G+A', y='market_value', color='G+A_per_90_min', hover_data=['Player_name','season'],trendline="ols", title= "Player Market Value vs G+A")
+        
+        
+        st.plotly_chart(fig_g_a_vs_mkt_value)
+        st.write(df_player_valuations[['Player_name','country_of_birth','position','season','market_value','games_played','minutes_played','goals','assists','G+A','G+A_per_90_min']].sort_values('G+A', ascending= False))
+        
